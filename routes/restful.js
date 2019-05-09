e57074581872030bed95f906d53b2dc7360b72b3
rocky-191 <1375022007@qq.com>
2019-05-09 16:30:30 +0800

    koa后台配合上传实例

--- routes/restful.js
+++ routes/restful.js
@@ -83,4 +83,12 @@ router.delete("/:id", ctx => {
     ctx.body = { ok: 1 };
 });
 
+// 文件上传
+const upload = require("koa-multer")({ dest: "./public/images" });
+router.post("/upload", upload.single("file"), ctx => {
+  console.log(ctx.req.file); // 注意数据存储在原始请求中
+  console.log(ctx.req.body); // 注意数据存储在原始请求中
+  ctx.body = "上传成功";
+});
+
 module.exports=router
